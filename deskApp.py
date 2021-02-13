d91ca57202fbf6b65d0f0c86cc154a89f78d2231
cescobarete <46694783+cescobarete@users.noreply.github.com>
2021-02-12 19:06:20 -0600

    Combine main and other soft

---
+++ deskApp.py
@@ -0,0 +1,5 @@
+from tkinter import *
+import tkinter.messagebox as MessageBox
+import mysql.connector as mysql
+
+root = Tk()
--- managmentDatabase.sql
+++ managmentDatabase.sql
@@ -6,14 +6,31 @@ CREATE DATABASE IF NOT EXISTS ManageEmp;
 USE ManageEmp;
 
 /* Drops the tables if doesnt exist*/
-drop table if exists Employee;
 drop table if exists PersonalInfo;
 drop table if exists Company;
+drop table if exists Employee;
 
 /* creates tables for each variable*/
-create table Employee(eID int, name varchar(25), startTime time, endTime time);
-create table Company(pID int, position varchar(25), postionTaken char(3));
-create table PersonalInfo(eID int, pID int, yearlySalary int, email varchar(100), review text);
+create table Employee(eID int NOT NULL,
+  name varchar(25) NOT NULL,
+  startTime time,
+  endTime time,
+  PRIMARY KEY(eID));
+
+create table Company(
+  pID int NOT NULL,
+  position varchar(25) NOT NULL,
+  postionTaken char(3),
+  directive text,
+  PRIMARY KEY(pID));
+
+create table PersonalInfo(eID int NOT NULL,
+  pID int NOT NULL,
+  yearlySalary int,
+  email varchar(100),
+  review text,
+  FOREIGN KEY(eID) REFERENCES Employee(eID),
+  FOREIGN KEY(pID) REFERENCES Company(pID));
 
 /* insert data into Employee table*/
 insert into Employee values(1000, 'Christian Escobarete', '07:00:00', '15:00:00');
@@ -23,13 +40,13 @@ insert into Employee values(1003, 'Luis Vazquez', '08:00:00', '16:00:00');
 insert into Employee values(1004, 'Ada Lovelace', '08:00:00', '16:00:00');
 
 /* insert table into Company table*/
-insert into Company values(1, 'Developer', 'yes');
-insert into Company values(2, 'Developer', 'yes');
-insert into Company values(3, 'Help Desk', 'yes');
-insert into Company values(4, 'Senior Devloper', 'yes');
-insert into Company values(5, 'Manager', 'yes');
-insert into Company values(6, 'Developer', 'no');
-insert into Company values(7, 'Information Technology', 'no');
+insert into Company values(1, 'Developer', 'yes','Create a project that will change the world');
+insert into Company values(2, 'Developer', 'yes','Create a porject that will change the world');
+insert into Company values(3, 'Help Desk', 'yes','Answer the phone');
+insert into Company values(4, 'Senior Devloper', 'yes','Create a porject that will chnage the world');
+insert into Company values(5, 'Manager', 'yes','Manage the employees and make sure things get done');
+insert into Company values(6, 'Developer', 'no', NULL);
+insert into Company values(7, 'Information Technology', 'no', NULL);
 
 /* insert table data PersonalInfo into table*/
 insert into PersonalInfo values(1000, 1, 50000, 'cescobarete@business.com', 'A terrible employee');
@@ -37,6 +54,10 @@ insert into PersonalInfo values(1001, 5, 70000, 'jsmith@business.com', 'Exemplor
 insert into PersonalInfo values(1002, 3, 30000, 'dmartinez@business.com', 'Does not work well with others');
 insert into PersonalInfo values(1003, 4, 80000, 'lvazquez@business.com', 'Smiles all the time');
 insert into PersonalInfo values(1004, 2, 50000, 'alovelace@business.com', 'Sketchy');
+
+/* retrieve data very quickly by indexing*/
+CREATE INDEX Employee
+ON Employee(name, startTime, endTime);
 
 /* Create user and privilges*/
 CREATE USER ms_user@localhost IDENTIFIED BY 'manageuser';
