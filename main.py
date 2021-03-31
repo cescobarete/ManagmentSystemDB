857ab48ad4e33281c8430bb795d7dbc8b9c3b455
cescobarete <46694783+cescobarete@users.noreply.github.com>
2021-03-31 11:44:36 -0500

    update

--- main.py
+++ main.py
@@ -11,35 +11,35 @@ def open_employee():
 
 root = tk.Tk()
 root.title('Desktop App')
+root.config(bg='white')
 root.resizable(False,False)
 
 canvas = tk.Canvas(root, width=600, height=300)
 canvas.grid(columnspan=3, rowspan=3)
 
-logo2 = Image.open('logo2.png')
-logo2 = logo2.resize((100,700), Image.ANTIALIAS)
+logo2 = Image.open('boss.png')
+logo2 = logo2.resize((65,65), Image.ANTIALIAS)
 logo2 = ImageTk.PhotoImage(logo2)
 logo_label2 = tk.Label(image=logo2)
 logo_label2.image = logo2
-logo_label2.place(x=0,y=0)
+logo_label2.place(x=270,y=220)
 
-logo3 = Image.open('logo2.png')
-logo3 = logo3.resize((100,700), Image.ANTIALIAS)
+logo3 = Image.open('personel.png')
 logo3 = ImageTk.PhotoImage(logo3)
 logo_label3 = tk.Label(image=logo3)
 logo_label3.image = logo3
-logo_label3.place(x=500,y=0)
+logo_label3.place(x=-70,y=150)
 
-logo = Image.open('logo.png')
+logo = Image.open('design.png')
 logo = ImageTk.PhotoImage(logo)
 logo_label = tk.Label(image=logo)
 logo_label.image = logo
-logo_label.grid(column=1, row=0)
+logo_label.place(x=200,y=0)
 
 openUser = tk.Button(root, text="Personal Information", font=('italic',20), highlightbackground='#3E4149', command=open_employee)
-openUser.place(x=130,y=200)
+openUser.place(x=50,y=240)
 
 openManage = tk.Button(root, text="Management", font=('italic',20), highlightbackground='#3E4149', command=open_manage)
-openManage.place(x=330,y=200)
+openManage.place(x=330,y=240)
 
 root.mainloop()
